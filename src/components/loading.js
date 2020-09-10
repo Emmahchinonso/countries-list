394b7c94ed0a384859772e705fc5bc3b8bba599d
Emmanuel chinonso <emmanuelchinons9@gmail.com>
2020-09-10 20:34:28 +0100

    correct mobile styling

--- src/components/loading.js
+++ src/components/loading.js
@@ -7,7 +7,11 @@ const Spinner = styled.div`
   position: absolute;
   left: 50%;
   top: 50%;
-  transform: translate(-50%, -50%)
+  transform: translate(-50%, -50%);
+
+  @media(max-width: 678px){
+    top: 65%;
+  }
 `;
 
 const DoubleBounce1 = styled.div`
