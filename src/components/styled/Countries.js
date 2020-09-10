953def61e9c189fea9ae1f542f492a477052c113
Emmanuel chinonso <emmanuelchinons9@gmail.com>
2020-09-10 21:01:51 +0100

    add netlify options

--- src/components/styled/Countries.js
+++ src/components/styled/Countries.js
@@ -9,6 +9,7 @@ export const Countries = styled.main`
 export const CountriesContainer = styled.div`
 	width: 100%;
 	display: grid;
-	grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  grid-gap: 2.5rem;
 	gap: 2.5rem;
 `;
